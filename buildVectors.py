ca0231bc55c51018b8123e679c812244647e759c
Justin Whitfield <justinwhitfieldd@gmail.com>
2024-02-08 16:20:23 -0600

    gather text from pdf's

--- buildVectors.py
+++ buildVectors.py
@@ -1,14 +1,19 @@
-#TODO
 import pandas as pd
 import tiktoken
 # consider https://pypi.org/project/embeddings-util/
 from PyPDF2 import PdfReader 
+import os
 from utils.embeddings_utils import get_embedding
 embedding_model = "text-embedding-3-small"
 embedding_encoding = "cl100k_base"
 max_tokens = 8000  # the maximum for text-embedding-3-small is 8191
 
-# load & inspect dataset
-# replace read_pdf with https://www.geeksforgeeks.org/extract-text-from-pdf-file-using-python/
-df = PdfReader('exampleData/01-CSE4283-Orientation-SE-Recap.pdf')
-print(df.pages[2].extract_text())
+# Get text from all files in exampleData folder
+for filename in os.listdir('exampleData'):
+    filepath = os.path.join('exampleData', filename)
+    df = PdfReader(filepath)
+    for page in df.pages:
+        print(page.extract_text())
+
+# TODO
+        # after seperating text from pdf now build into vectors
