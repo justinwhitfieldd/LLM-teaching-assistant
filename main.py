c99c8b8de3fab5eea74993695a18e374731558c5
Raj Patel <rkp88@msstate.edu>
2024-03-27 17:04:27 -0500

    Added Persistent Prompt and make minor changes to UI

--- main.py
+++ main.py
@@ -3,68 +3,7 @@ import streamlit.components.v1 as components
 import initializeLLM as LLM
 import base64
 
-# # Added the PATENT Lab banner on the top
-# st.image("Resources/banner-new.png", caption=None, width=None, use_column_width="auto", clamp=False, channels="RGB", output_format="auto")
-# st.divider()
-
-# # Here we added the title of the app with the logo
-# col1, col2, col3 = st.columns([2,1,6])
-# with col2:
-#     st.image("Resources/logo.png", caption=None, width=None, use_column_width="auto", clamp=False, channels="RGB", output_format="auto")
-# with col3:
-#     st.header("Bully - Teaching Assistant")
-# st.divider()
-
-# # Introduced the bot application
-# col1, col2, col3 = st.columns([1,9,2])
-# with col1:
-#     st.image("Resources/logo.png", caption=None, width=None, use_column_width="auto", clamp=False, channels="RGB", output_format="auto")
-# with col2:
-#     code = '''Hello bulldawgs! I am a virtual teaching assistant for
-# Introduction to Computer Programming. In order for me to
-# best serve you, please choose one of the following prompt.'''
-#     st.code(code, language=None)
-
-# st.write("")
-# st.write("")
-
-# # Added the prompt for the user to select and ask further questions about
-# col1, col2 = st.columns([2,1])
-# with col1:
-#     userSelectedPrompt = st.selectbox(
-#         "What can I help you today with?", key="option", options=["Explain Concept", "Code Review", "Take a Quiz"], index=0)
-
-# # This markdown is for the user and assistant to reverse the direction of the text-alignment
-# # st.markdown(
-# #     """ <style>
-# #             .st-emotion-cache-janbn0 {
-# #             flex-direction: row-reverse;
-# #             text-align: right;}
-# #         </style>""", unsafe_allow_html=True)
-
-# # Initialize chat history
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-
-# # Display chat messages from history on app rerun
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
-
-# # React to user input
-# if prompt := st.chat_input("What is up?"):
-#     # Display user message in chat message container
-#     st.chat_message("user").markdown(prompt)
-#     # Add user message to chat history
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-
-#     # generate LLM response
-#     response = LLM.get_response_wFunction(prompt)
-#     # Display assistant response in chat message container
-#     with st.chat_message("assistant"):
-#         st.markdown(response)
-#     # Add assistant response to chat history
-#     st.session_state.messages.append({"role": "assistant", "content": response})
+userSelectedPrompt = None
 
 def load_css():
     with open("Resources/styles.css", "r") as f:
@@ -78,8 +17,12 @@ def initialize_session_state():
 def on_click_callback():
     human_prompt = st.session_state.human_prompt
     st.session_state.history.append({"role": "user", "content": human_prompt})
-    response = LLM.get_response_wFunction(human_prompt)
-    st.session_state.history.append({"role": "assistant", "content": response})
+    if userSelectedPrompt == None:
+        response = "Please select one of the prompt before I can proceed to help you!"
+        st.session_state.history.append({"role": "assistant", "content": response})
+    else:
+        response = LLM.get_response_wFunction(human_prompt)
+        st.session_state.history.append({"role": "assistant", "content": response})
 
 load_css()
 initialize_session_state()
@@ -102,18 +45,9 @@ with col1:
     st.image("Resources/ai_icon.png", caption=None, width=None, use_column_width="auto", clamp=False, channels="RGB", output_format="auto")
 with col2:
     code = '''Hello bulldawgs! I am a virtual teaching assistant for
-Introduction to Computer Programming. In order for me to
-best serve you, please choose one of the following prompt.'''
+Introduction to Computer Programming.'''
     st.code(code, language=None)
-
-st.write("")
-st.write("")
-
-# Added the prompt for the user to select and ask further questions about
-col1, col2 = st.columns([2,1])
-with col1:
-    userSelectedPrompt = st.selectbox(
-        "What can I help you today with?", key="option", options=["Explain Concept", "Code Review", "Take a Quiz"], index=0)
+    userSelectedPrompt = st.radio("In order for me to best serve you, please choose one of the following prompt:", ["Explain Concept", "Take a Quiz"],index=None,)
 
 chat_placeholder = st.container()
 prompt_placeholder = st.form("chat-form")
@@ -150,11 +84,10 @@ with chat_placeholder:
         st.markdown("")
 
 with prompt_placeholder:
-    st.markdown("**Chat**")
     cols = st.columns((6, 1))
     cols[0].text_input(
         "Chat",
-        value="What's up!",
+        value="Type your message here to Chatbot-TA!",
         label_visibility="collapsed",
         key="human_prompt",
     )
@@ -163,27 +96,3 @@ with prompt_placeholder:
         type="primary", 
         on_click=on_click_callback, 
     )
-
-components.html("""
-<script>
-const streamlitDoc = window.parent.document;
-
-const buttons = Array.from(
-    streamlitDoc.querySelectorAll('.stButton > button')
-);
-const submitButton = buttons.find(
-    el => el.innerText === 'Submit'
-);
-
-streamlitDoc.addEventListener('keydown', function(e) {
-    switch (e.key) {
-        case 'Enter':
-            submitButton.click();
-            break;
-    }
-});
-</script>
-""", 
-    height=0,
-    width=0,
-)
